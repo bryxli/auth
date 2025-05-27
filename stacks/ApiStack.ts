a05d38ad8110cb0a576b8fd74063fc226a824aa8
bryxli <bryanzero765@gmail.com>
2025-05-27 02:23:12 -0500

    Add new routes to ApiStack

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -23,6 +23,18 @@ export function API({ stack }: StackContext) {
        * Returns a simple health check response.
        */
       "GET /": "packages/functions/src/health.handler",
+
+      /**
+       * @route POST /user/authenticate
+       * Creates a new authentication request.
+       */
+      "POST /authenticate": "packages/functions/src/auth.authenticate",
+
+      /**
+       * @route POST /user/register
+       * Creates a new user account using default type credentials.
+       */
+      "POST /register": "packages/functions/src/auth.register",
     },
   });
 
